5802800a6228d92fa175c542f16b7aa2265326d0
mehov <mehov@users.noreply.github.com>
2024-11-18 23:22:45 +0000

    Remove unnecessary bootstrap() declaration

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,14 +3,8 @@
 namespace CakePHPOpencart;
 
 use Cake\Core\BasePlugin;
-use Cake\Core\PluginApplicationInterface;
 
 class Plugin extends BasePlugin
 {
-
-    public function bootstrap(PluginApplicationInterface $app)
-    {
-        parent::bootstrap($app);
-    }
 
 }
