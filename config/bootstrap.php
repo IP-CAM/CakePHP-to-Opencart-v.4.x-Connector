286b85e0a26d47c624ba055a5cf0ca0d7fb2412c
mehov <mehov@users.noreply.github.com>
2024-11-18 23:38:47 +0000

    Pick defaultCart on plugin boot if empty

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,3 +2,10 @@
 
 // Try loading configuration for the plugin from app config folder
 \Cake\Core\Configure::load($this->getName(), 'default');
+
+// Pick the first configured site to be the default
+if (!\Cake\Core\Configure::check($this->getName().'.defaultCart')) {
+    $cartList = \Cake\Core\Configure::read($this->getName().'.cartList');
+    $cartSymbol = array_keys($cartList)[0];
+    \Cake\Core\Configure::write($this->getName().'.defaultCart', $cartSymbol);
+}
